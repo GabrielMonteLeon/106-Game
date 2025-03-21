b6868405ba0a815c49a2960cbf09c83d92367c42
dfs5974 <dfs5974@g.rit.edu>
2025-03-21 13:04:02 -0400

    starter code

---
+++ MEDU/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDU
+{
+    internal abstract class GameObject
+    {
+        //fields
+        private Rectangle position;
+        private Texture2D texture;
+    }
+}
---
+++ MEDU/Level.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDU
+{
+    internal class Level
+    {
+        //fields
+        private List<Platform> platforms;
+        private Rectangle endTrigger;
+
+
+
+    }
+}
