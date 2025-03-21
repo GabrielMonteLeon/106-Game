9aa6658157f5a5cb25a911bc6d452c1da9438b30
Daniel Maher (RIT Student) <djm2518@rit.edu>
2025-03-21 13:39:38 -0400

    Create Level/Platform Constructors

--- MEDU/Platform.cs
+++ MEDU/Platform.cs
@@ -12,5 +12,14 @@ namespace MEDU
     {
         private bool passThrough;
         private bool isSafe;
+
+        public bool PassThrough => passThrough;
+        public bool IsSafe => isSafe;
+
+        public Platform(Rectangle position, Texture2D texture, bool passThrough, bool isSafe) : base(position, texture)
+        {
+            this.passThrough = passThrough;
+            this.isSafe = isSafe;
+        }
     }
 }
